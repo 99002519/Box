fbdf1d159e23c7fe549c9e21b3f4119e4dd01c97
99002519 <71540886+99002519@users.noreply.github.com>
2020-10-05 13:42:21 +0530

    Add files via upload

---
+++ pch.h
@@ -0,0 +1,16 @@
+class Box
+{
+private:
+	int m_length, m_height, m_breadth;
+public:
+	Box();
+	Box(int);
+	Box(int, int, int);
+	Box(const Box&);
+	int length();
+	int breadth();
+	int height();
+	int volume();
+	void display();
+
+};
